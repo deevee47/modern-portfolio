22cc8aab3a3fb3edd11f6685075f76a7fe33cdbb
Divyansh Vishwakarma <divyanshvishwakarma4@gmail.com>
2024-05-27 20:33:43 +0530

    bug fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      {/* UI: Spotlights */}
+
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -20,7 +20,7 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      {/* UI: grid */}
+
       <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
