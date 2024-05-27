621cda62fc211519eb2e60042d703808dad1b5ca
Divyansh Vishwakarma <divyanshvishwakarma4@gmail.com>
2024-05-27 20:13:15 +0530

    vercel analystics deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +25,8 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+          {children}
+          <Analytics />
           </ThemeProvider></body>
     </html>
   );
